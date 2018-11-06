627416d6bc4e0e3eca2dab5a9b20ba78b07b5743
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-06 22:47:46 +0100

    Refactor point constructor tests

--- test/point/ctor.cpp
+++ test/point/ctor.cpp
@@ -2,44 +2,53 @@
 #include <doctest.h>
 #include <rmath/point.hpp>
 
-TEST_CASE("upoint2 can be constructed from scalar") {
+TEST_CASE_TEMPLATE("unsigned point can be constructed from scalar",
+                   T,
+                   rmath::upt2,
+                   rmath::upt3,
+                   rmath::upt4) {
     std::vector<uint32_t> data {0, 1, 2, 30, 199};
 
     for (auto& d : data) {
         CAPTURE(d);
 
-        auto pt = rmath::upt2(d);
-
-        CHECK(pt.x == d);
-        CHECK(pt.y == d);
+        auto pt = T(d);
+        for (size_t i = 0; i < T::size(); ++i) {
+            CHECK(pt.data[i] == d);
+        }
     }
 }
 
-TEST_CASE("upoint3 can be constructed from scalar") {
-    std::vector<uint32_t> data {0, 1, 2, 30, 199};
+TEST_CASE_TEMPLATE("signed point can be constructed from scalar",
+                   T,
+                   rmath::ipt2,
+                   rmath::ipt3,
+                   rmath::ipt4) {
+    std::vector<int32_t> data {0, 1, -2, 30, -199};
 
     for (auto& d : data) {
         CAPTURE(d);
 
-        auto pt = rmath::upt3(d);
-
-        CHECK(pt.x == d);
-        CHECK(pt.y == d);
-        CHECK(pt.z == d);
+        auto pt = T(d);
+        for (size_t i = 0; i < T::size(); ++i) {
+            CHECK(pt.data[i] == d);
+        }
     }
 }
 
-TEST_CASE("upoint4 can be constructed from scalar") {
-    std::vector<uint32_t> data {0, 1, 2, 30, 199};
+TEST_CASE_TEMPLATE("float point can be constructed from scalar",
+                   T,
+                   rmath::pt2,
+                   rmath::pt3,
+                   rmath::pt4) {
+    std::vector<float> data {0.0f, 1.33f, -2.5f, 30.0f, -199.005f};
 
     for (auto& d : data) {
         CAPTURE(d);
 
-        auto pt = rmath::upt4(d);
-
-        CHECK(pt.x == d);
-        CHECK(pt.y == d);
-        CHECK(pt.z == d);
-        CHECK(pt.w == d);
+        auto pt = T(d);
+        for (size_t i = 0; i < T::size(); ++i) {
+            CHECK(pt.data[i] == doctest::Approx(d));
+        }
     }
 }
