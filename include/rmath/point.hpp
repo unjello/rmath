b06258e1f2239fc461b2088d7722c0921f554d15
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-09 21:59:01 +0100

    Refactor point and vector definitions

--- include/rmath/point.hpp
+++ include/rmath/point.hpp
@@ -9,14 +9,20 @@ struct point_tag {};
 
 template <size_t L, typename T>
 using point = base<L, T, detail::point_tag>;
+template <typename T>
+using point2 = point<2, T>;
+template <typename T>
+using point3 = point<3, T>;
+template <typename T>
+using point4 = point<4, T>;
 
-using upt2 = point<2, uint32_t>;
-using upt3 = point<3, uint32_t>;
-using upt4 = point<4, uint32_t>;
-using ipt2 = point<2, int32_t>;
-using ipt3 = point<3, int32_t>;
-using ipt4 = point<4, int32_t>;
-using pt2  = point<2, float>;
-using pt3  = point<3, float>;
-using pt4  = point<4, float>;
+using upt2 = point2<uint32_t>;
+using upt3 = point3<uint32_t>;
+using upt4 = point4<uint32_t>;
+using ipt2 = point2<int32_t>;
+using ipt3 = point3<int32_t>;
+using ipt4 = point4<int32_t>;
+using pt2  = point2<float>;
+using pt3  = point3<float>;
+using pt4  = point4<float>;
 }
--- include/rmath/vector.hpp
+++ include/rmath/vector.hpp
@@ -9,14 +9,20 @@ struct vector_tag {};
 
 template <size_t L, typename T>
 using vector = base<L, T, detail::vector_tag>;
+template <typename T>
+using vector2 = vector<2, T>;
+template <typename T>
+using vector3 = vector<3, T>;
+template <typename T>
+using vector4 = vector<4, T>;
 
-using uvec2 = vector<2, uint32_t>;
-using uvec3 = vector<3, uint32_t>;
-using uvec4 = vector<4, uint32_t>;
-using ivec2 = vector<2, int32_t>;
-using ivec3 = vector<3, int32_t>;
-using ivec4 = vector<4, int32_t>;
-using vec2  = vector<2, float>;
-using vec3  = vector<3, float>;
-using vec4  = vector<4, float>;
+using uvec2 = vector2<uint32_t>;
+using uvec3 = vector3<uint32_t>;
+using uvec4 = vector4<uint32_t>;
+using ivec2 = vector2<int32_t>;
+using ivec3 = vector3<int32_t>;
+using ivec4 = vector4<int32_t>;
+using vec2  = vector2<float>;
+using vec3  = vector3<float>;
+using vec4  = vector4<float>;
 }
