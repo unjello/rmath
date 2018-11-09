df0cf97d16247eba3a724e789373ebf7c5732224
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-09 18:18:37 +0100

    Refactor point class into a base class.
    Vector, Color and Point share base data functions,
    but don't want to use inheritance here, so that the types
    are not substitutable.

---
+++ include/rmath/base.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+
+namespace rmath {
+
+template <size_t L, typename T, typename Tag>
+struct base;
+
+template <typename T, typename Tag>
+struct base<2, T, Tag> {
+    typedef T value_type;
+
+    union {
+        struct {
+            T x, y;
+        };
+        T data[2];
+    };
+
+    constexpr base()                       = default;
+    constexpr base(base<2, T, Tag> const&) = default;
+
+    constexpr explicit base(T scalar)
+        : x(scalar)
+        , y(scalar) {}
+    constexpr base(T _x, T _y)
+        : x(_x)
+        , y(_y) {}
+
+    static constexpr size_t size() { return 2; }
+};
+
+template <typename T, typename Tag>
+struct base<3, T, Tag> {
+    typedef T value_type;
+
+    union {
+        struct {
+            T x, y, z;
+        };
+        T data[4];
+    };
+
+    constexpr base()                       = default;
+    constexpr base(base<3, T, Tag> const&) = default;
+
+    constexpr explicit base(T scalar)
+        : x(scalar)
+        , y(scalar)
+        , z(scalar) {}
+    constexpr base(T _x, T _y, T _z)
+        : x(_x)
+        , y(_y)
+        , z(_z) {}
+
+    static constexpr size_t size() { return 3; }
+};
+
+template <typename T, typename Tag>
+struct base<4, T, Tag> {
+    typedef T value_type;
+
+    union {
+        struct {
+            T x, y, z, w;
+        };
+        T data[4];
+    };
+
+    constexpr base()                       = default;
+    constexpr base(base<4, T, Tag> const&) = default;
+
+    constexpr explicit base(T scalar)
+        : x(scalar)
+        , y(scalar)
+        , z(scalar)
+        , w(scalar) {}
+    constexpr base(T _x, T _y, T _z, T _w)
+        : x(_x)
+        , y(_y)
+        , z(_z)
+        , w(_w) {}
+
+    static constexpr size_t size() { return 4; }
+};
+}
--- include/rmath/point.hpp
+++ include/rmath/point.hpp
@@ -1,92 +1,14 @@
 #pragma once
-#include <cstddef>
-#include <cstdint>
+
+#include <rmath/base.hpp>
 
 namespace rmath {
+namespace detail {
+struct point_tag {};
+}
 
 template <size_t L, typename T>
-struct point;
-
-template <typename T>
-struct point<2, T> {
-    typedef T           value_type;
-    typedef point<2, T> type;
-
-    union {
-        struct {
-            T x, y;
-        };
-        T data[2];
-    };
-
-    constexpr point()                   = default;
-    constexpr point(point<2, T> const&) = default;
-
-    constexpr explicit point(T scalar)
-        : x(scalar)
-        , y(scalar) {}
-    constexpr point(T _x, T _y)
-        : x(_x)
-        , y(_y) {}
-
-    static constexpr size_t size() { return 2; }
-};
-
-template <typename T>
-struct point<3, T> {
-    typedef T           value_type;
-    typedef point<3, T> type;
-
-    union {
-        struct {
-            T x, y, z;
-        };
-        T data[4];
-    };
-
-    constexpr point()                   = default;
-    constexpr point(point<3, T> const&) = default;
-
-    constexpr explicit point(T scalar)
-        : x(scalar)
-        , y(scalar)
-        , z(scalar) {}
-    constexpr point(T _x, T _y, T _z)
-        : x(_x)
-        , y(_y)
-        , z(_z) {}
-
-    static constexpr size_t size() { return 3; }
-};
-
-template <typename T>
-struct point<4, T> {
-    typedef T           value_type;
-    typedef point<4, T> type;
-
-    union {
-        struct {
-            T x, y, z, w;
-        };
-        T data[4];
-    };
-
-    constexpr point()                   = default;
-    constexpr point(point<4, T> const&) = default;
-
-    constexpr explicit point(T scalar)
-        : x(scalar)
-        , y(scalar)
-        , z(scalar)
-        , w(scalar) {}
-    constexpr point(T _x, T _y, T _z, T _w)
-        : x(_x)
-        , y(_y)
-        , z(_z)
-        , w(_w) {}
-
-    static constexpr size_t size() { return 4; }
-};
+using point = base<L, T, detail::point_tag>;
 
 using upt2 = point<2, uint32_t>;
 using upt3 = point<3, uint32_t>;
