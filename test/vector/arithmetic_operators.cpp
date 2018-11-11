4d974e5bd24909e73991d7de402b9cb00255d10f
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-11 19:58:29 +0100

    Add tests for all arithmetic + operator types

--- test/vector/arithmetic_operators.cpp
+++ test/vector/arithmetic_operators.cpp
@@ -3,7 +3,7 @@
 #include <rmath/vector.hpp>
 
 TEST_CASE("two uvec2's are added correctly") {
-    rmath::uvec2 a{10, 20}, b{30, 10};
+    rmath::uvec2 a {10, 20}, b {30, 10};
 
     auto c = a + b;
 
@@ -13,4 +13,39 @@ TEST_CASE("two uvec2's are added correctly") {
     CHECK(b.y == 10);
     CHECK(c.x == 40);
     CHECK(c.y == 30);
+}
+
+TEST_CASE("two ivec3's are added correctly") {
+    rmath::ivec3 a {10, 20, -30}, b {30, 10, 40};
+
+    auto c = a + b;
+
+    CHECK(a.x == 10);
+    CHECK(a.y == 20);
+    CHECK(a.z == -30);
+    CHECK(b.x == 30);
+    CHECK(b.y == 10);
+    CHECK(b.z == 40);
+    CHECK(c.x == 40);
+    CHECK(c.y == 30);
+    CHECK(c.z == 10);
+}
+
+TEST_CASE("two vec4's are added correctly") {
+    rmath::vec4 a {10, 20, -10.5, -20}, b {30, 10, -10.5, 10.88};
+
+    auto c = a + b;
+
+    CHECK(a.x == doctest::Approx(10));
+    CHECK(a.y == doctest::Approx(20));
+    CHECK(a.z == doctest::Approx(-10.5));
+    CHECK(a.w == doctest::Approx(-20));
+    CHECK(b.x == doctest::Approx(30));
+    CHECK(b.y == doctest::Approx(10));
+    CHECK(b.z == doctest::Approx(-10.5));
+    CHECK(b.w == doctest::Approx(10.88));
+    CHECK(c.x == doctest::Approx(40));
+    CHECK(c.y == doctest::Approx(30));
+    CHECK(c.z == doctest::Approx(-21));
+    CHECK(c.w == doctest::Approx(-9.12));
 }
