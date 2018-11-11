1e47dd9d659d3d673bafd9706a9846771767b1ee
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-11 19:46:12 +0100

    Cover all vector types for length function

--- test/vector/length.cpp
+++ test/vector/length.cpp
@@ -19,3 +19,39 @@ TEST_CASE("vec2 length is correctly calculated") {
 
     CHECK(rmath::length(v) == doctest::Approx(22.808167f));
 }
+
+TEST_CASE("uvec3 length is correctly calculated") {
+    rmath::uvec3 v {10, 20, 30};
+
+    CHECK(rmath::length(v) == 37);
+}
+
+TEST_CASE("ivec3 length is correctly calculated") {
+    rmath::ivec3 v {10, -20, 30};
+
+    CHECK(rmath::length(v) == 37);
+}
+
+TEST_CASE("vec3 length is correctly calculated") {
+    rmath::vec3 v {10.10f, -20.45f, 33.33f};
+
+    CHECK(rmath::length(v) == doctest::Approx(40.386896f));
+}
+
+TEST_CASE("uvec4 length is correctly calculated") {
+    rmath::uvec4 v {10, 20, 30, 53};
+
+    CHECK(rmath::length(v) == 65);
+}
+
+TEST_CASE("ivec4 length is correctly calculated") {
+    rmath::ivec4 v {10, -20, 30, -53};
+
+    CHECK(rmath::length(v) == 65);
+}
+
+TEST_CASE("vec4 length is correctly calculated") {
+    rmath::vec4 v {10.10f, -20.45f, 33.33f, 55.41f};
+
+    CHECK(rmath::length(v) == doctest::Approx(68.566533f));
+}
