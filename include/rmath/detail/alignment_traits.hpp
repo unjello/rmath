186621f7d657846fdf6ef1c56d894f118e312391
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-12-26 22:19:08 +0100

    Add alignment helpers

---
+++ include/rmath/detail/alignment_traits.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+
+namespace rmath::detail {
+template <size_t L, size_t V>
+struct alignment_constant {
+    static constexpr size_t          value = V;
+    typedef L                        value_type;
+    typedef alignment_constant<L, V> type;
+    constexpr                        operator value_type() { return value; }
+};
+
+template <size_t L>
+struct align_to : public alignment_constant<L, 8> {};
+template <>
+struct align_to<2> : public alignment_constant<2, 2> {};
+template <>
+struct align_to<3> : public alignment_constant<3, 4> {};
+template <>
+struct align_to<4> : public alignment_constant<4, 4> {};
+}
