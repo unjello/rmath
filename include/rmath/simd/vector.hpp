ff661e8974e885ef00a948fd86a55333eb11c04b
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-05 23:52:02 +0100

    Add first intrisincs for sse simd

--- include/rmath/simd/vector.hpp
+++ include/rmath/simd/vector.hpp
@@ -7,6 +7,8 @@
 namespace rmath::simd {
 
 struct vector4 {
+    explicit vector4(float x, float y, float z, float w) { data = _mm_set_ps(w, z, y, x); }
+    explicit vector4(float v) { data = _mm_set_ps1(v); }
     __m128 data;
 };
 
