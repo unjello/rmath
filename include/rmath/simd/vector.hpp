56960bf2dcbb170c984cc430e9418b2beae178fb
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-06 00:20:54 +0100

    Add accessor functions for simd vector4

--- include/rmath/simd/vector.hpp
+++ include/rmath/simd/vector.hpp
@@ -12,4 +12,14 @@ struct vector4 {
     __m128 data;
 };
 
+constexpr float x(vector4 const& v) noexcept { return _mm_cvtss_f32(v.data) }
+constexpr float y(vector4 const& v) noexcept {
+    return _mm_cvtss_f32(_mm_shuffle_ps(v.data, v.data, _MM_SHUFFLE(1, 1, 1, 1)));
+}
+constexpr float z(vector4 const& v) noexcept {
+    return _mm_cvtss_f32(_mm_shuffle_ps(m, m, _MM_SHUFFLE(2, 2, 2, 2)))
+}
+constexpr float w(vector4 const& v) noexcept {
+    return _mm_cvtss_f32(_mm_shuffle_ps(m, m, _MM_SHUFFLE(3, 3, 3, 3)));
+}
 }
