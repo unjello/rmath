442aa5f3871b6564456d17740c8bd34083612ab0
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-07 21:53:46 +0100

    Add explicit constructor to create float4 directly from SSE result

--- include/rmath/simd/vector.hpp
+++ include/rmath/simd/vector.hpp
@@ -9,6 +9,7 @@ namespace rmath::simd {
 struct vector4 {
     explicit vector4(float x, float y, float z, float w) { data = _mm_set_ps(w, z, y, x); }
     explicit vector4(float v) { data = _mm_set_ps1(v); }
+    explicit vector4(__m128 d) { data = d; }
     __m128 data;
 };
 
@@ -22,4 +23,5 @@ constexpr float z(vector4 const& v) noexcept {
 constexpr float w(vector4 const& v) noexcept {
     return _mm_cvtss_f32(_mm_shuffle_ps(m, m, _MM_SHUFFLE(3, 3, 3, 3)));
 }
+
 }
