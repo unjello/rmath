a64f31ee297d0fc7b232d5bfc1ab77ab7a0cd3a2
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-11 02:02:36 -0600

    Add operators for vector vs scalar

--- include/rmath/simd/vector.hpp
+++ include/rmath/simd/vector.hpp
@@ -13,19 +13,23 @@ struct vector4 {
     __m128 data;
 };
 
-float x(vector4 const& v) noexcept { return _mm_cvtss_f32(v.data); }
-float y(vector4 const& v) noexcept {
+inline float x(vector4 const& v) noexcept { return _mm_cvtss_f32(v.data); }
+inline float y(vector4 const& v) noexcept {
     return _mm_cvtss_f32(_mm_shuffle_ps(v.data, v.data, _MM_SHUFFLE(1, 1, 1, 1)));
 }
-float z(vector4 const& v) noexcept {
+inline float z(vector4 const& v) noexcept {
     return _mm_cvtss_f32(_mm_shuffle_ps(v.data, v.data, _MM_SHUFFLE(2, 2, 2, 2)));
 }
-float w(vector4 const& v) noexcept {
+inline float w(vector4 const& v) noexcept {
     return _mm_cvtss_f32(_mm_shuffle_ps(v.data, v.data, _MM_SHUFFLE(3, 3, 3, 3)));
 }
 
-vector4 operator+(vector4 a, vector4 b) { return vector4(_mm_add_ps(a.data, b.data)); }
-vector4 operator-(vector4 a, vector4 b) { return vector4(_mm_sub_ps(a.data, b.data)); }
-vector4 operator*(vector4 a, vector4 b) { return vector4(_mm_mul_ps(a.data, b.data)); }
-vector4 operator/(vector4 a, vector4 b) { return vector4(_mm_div_ps(a.data, b.data)); }
+inline vector4 operator+(vector4 a, vector4 b) { return vector4(_mm_add_ps(a.data, b.data)); }
+inline vector4 operator-(vector4 a, vector4 b) { return vector4(_mm_sub_ps(a.data, b.data)); }
+inline vector4 operator*(vector4 a, vector4 b) { return vector4(_mm_mul_ps(a.data, b.data)); }
+inline vector4 operator/(vector4 a, vector4 b) { return vector4(_mm_div_ps(a.data, b.data)); }
+inline vector4 operator*(vector4 a, float b) { return vector4(_mm_mul_ps(a.data, _mm_set_ps1(b))); }
+inline vector4 operator/(vector4 a, float b) { return vector4(_mm_div_ps(a.data, _mm_set_ps1(b))); }
+inline vector4 operator*(float a, vector4 b) { return vector4(_mm_mul_ps(_mm_set_ps1(a), b.data)); }
+inline vector4 operator/(float a, vector4 b) { return vector4(_mm_div_ps(_mm_set_ps1(a), b.data)); }
 }
