ae83f93e624d15f1b8c497a14f2928998c78b504
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-03-09 09:37:47 +0100

    Add [[nodiscard]]

--- include/rmath/generic/base.hpp
+++ include/rmath/generic/base.hpp
@@ -26,7 +26,7 @@ struct base {
         construct<0>(std::forward<value_type>(t0), tail...);
     }
 
-    constexpr static size_t size() noexcept { return component_size; }
+    [[nodiscard]] constexpr static size_t size() noexcept { return component_size; }
 
     template <size_t offset, class... Tail>
     constexpr void construct(value_type&& t0, Tail&&... tail) {
@@ -54,52 +54,52 @@ constexpr T& w(base<L, T, Tag>& v) noexcept {
     return v.data[3];
 }
 template <size_t L, typename T, typename Tag>
-constexpr T x(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr T x(base<L, T, Tag> const& v) noexcept {
     return v.data[0];
 }
 template <size_t L, typename T, typename Tag>
-constexpr T y(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr T y(base<L, T, Tag> const& v) noexcept {
     return v.data[1];
 }
 template <size_t L, typename T, typename Tag>
-constexpr T z(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr T z(base<L, T, Tag> const& v) noexcept {
     return v.data[2];
 }
 template <size_t L, typename T, typename Tag>
-constexpr T w(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr T w(base<L, T, Tag> const& v) noexcept {
     return v.data[3];
 }
 
 namespace detail {
 template <size_t L, typename T, typename U, typename Tag, typename F>
-constexpr base<L, T, Tag> op_binary(base<L, T, Tag> const& a, U b, F f) {
+[[nodiscard]] inline constexpr base<L, T, Tag> op_binary(base<L, T, Tag> const& a, U b, F f) {
     return op_binary(a, b, f, std::make_integer_sequence<size_t, L>());
 }
 
 template <size_t L, typename T, typename U, typename Tag, typename F, size_t... i>
-constexpr base<L, T, Tag>
+[[nodiscard]] inline constexpr base<L, T, Tag>
 op_binary(base<L, T, Tag> const& a, U b, F f, std::integer_sequence<size_t, i...>) {
     return base<L, T, Tag>{f(a.data[i], b)...};
 }
 
 template <size_t L, typename T, typename U, typename Tag, typename F>
-constexpr base<L, T, Tag> op_binary(U a, base<L, T, Tag> const& b, F f) {
+[[nodiscard]] inline constexpr base<L, T, Tag> op_binary(U a, base<L, T, Tag> const& b, F f) {
     return op_binary(a, b, f, std::make_integer_sequence<size_t, L>());
 }
 
 template <size_t L, typename T, typename U, typename Tag, typename F, size_t... i>
-constexpr base<L, T, Tag>
+[[nodiscard]] inline constexpr base<L, T, Tag>
 op_binary(U a, base<L, T, Tag> const& b, F f, std::integer_sequence<size_t, i...>) {
     return base<L, T, Tag>{f(a, b.data[i])...};
 }
 
 template <size_t L, typename T, typename Tag, typename F>
-constexpr base<L, T, Tag> op_unary(base<L, T, Tag> const& v, F f) {
+[[nodiscard]] inline constexpr base<L, T, Tag> op_unary(base<L, T, Tag> const& v, F f) {
     return op_unary(v, f, std::make_integer_sequence<size_t, L>());
 }
 
 template <size_t L, typename T, typename Tag, typename F, size_t... i>
-constexpr base<L, T, Tag>
+[[nodiscard]] inline constexpr base<L, T, Tag>
 op_unary(base<L, T, Tag> const& v, F f, std::integer_sequence<size_t, i...>) {
     return base<L, T, Tag>{f(v.data[i])...};
 }
@@ -112,12 +112,12 @@ op_unary(base<L, T, Tag> const& v, F f, std::integer_sequence<size_t, i...>) {
  * @{
  */
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator+(base<L, T, Tag> const& a, U b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator+(base<L, T, Tag> const& a, U b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 + _2; });
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator+(U a, base<L, T, Tag> const& b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator+(U a, base<L, T, Tag> const& b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 + _2; });
 }
 /** @} */
@@ -129,12 +129,12 @@ constexpr base<L, T, Tag> operator+(U a, base<L, T, Tag> const& b) noexcept {
  * @{
  */
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator-(base<L, T, Tag> const& a, U b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator-(base<L, T, Tag> const& a, U b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 - _2; });
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator-(U a, base<L, T, Tag> const& b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator-(U a, base<L, T, Tag> const& b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 - _2; });
 }
 /** @} */
@@ -146,12 +146,12 @@ constexpr base<L, T, Tag> operator-(U a, base<L, T, Tag> const& b) noexcept {
  * @{
  */
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator*(base<L, T, Tag> const& a, U b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator*(base<L, T, Tag> const& a, U b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 * _2; });
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator*(U a, base<L, T, Tag> const& b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator*(U a, base<L, T, Tag> const& b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 * _2; });
 }
 /** @} */
@@ -164,33 +164,33 @@ constexpr base<L, T, Tag> operator*(U a, base<L, T, Tag> const& b) noexcept {
  * @{
  */
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator/(base<L, T, Tag> const& a, U b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator/(base<L, T, Tag> const& a, U b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 / _2; });
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> operator/(U a, base<L, T, Tag> const& b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator/(U a, base<L, T, Tag> const& b) noexcept {
     return detail::op_binary(a, b, [](auto _1, auto _2) { return _2 / _2; });
 }
 /** @} */
 
 template <size_t L, typename T, typename Tag>
-constexpr base<L, T, Tag> operator+(base<L, T, Tag> const& a) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator+(base<L, T, Tag> const& a) noexcept {
     return base<L, T, Tag>{a};
 }
 
 template <size_t L, typename T, typename Tag>
-constexpr base<L, T, Tag> operator-(base<L, T, Tag> const& a) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> operator-(base<L, T, Tag> const& a) noexcept {
     return detail::op_unary(a, [](auto i) { return -i; });
 }
 
 template <size_t L, typename T, typename Tag>
-constexpr base<L, T, Tag> abs(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> abs(base<L, T, Tag> const& v) noexcept {
     return detail::op_unary(v, [](auto i) { return std::abs(i); });
 }
 
 template <size_t L, typename T, typename Tag>
-constexpr base<L, T, Tag> floor(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> floor(base<L, T, Tag> const& v) noexcept {
     if constexpr (std::is_integral_v<T> == true) {
         return v;
     } else {
@@ -212,7 +212,7 @@ constexpr base<L, T, Tag> floor(base<L, T, Tag> const& v) noexcept {
  * @see https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/fract.xhtml
  */
 template <size_t L, typename T, typename Tag>
-constexpr base<L, T, Tag> fract(base<L, T, Tag> const& v) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> fract(base<L, T, Tag> const& v) noexcept {
     if constexpr (std::is_integral_v<T> == true) {
         return 0;
     } else {
@@ -221,7 +221,7 @@ constexpr base<L, T, Tag> fract(base<L, T, Tag> const& v) noexcept {
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> mod(base<L, T, Tag> const& a, U b) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> mod(base<L, T, Tag> const& a, U b) noexcept {
     if constexpr (std::is_integral_v<T> == true) {
         return detail::op_binary(a, b, [](auto _1, auto _2) { return _1 % _2; });
 
@@ -231,12 +231,12 @@ constexpr base<L, T, Tag> mod(base<L, T, Tag> const& a, U b) noexcept {
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> sin(base<L, T, Tag> const& a) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> sin(base<L, T, Tag> const& a) noexcept {
     return detail::op_unary(a, [](auto _1) { return std::sin(_1); });
 }
 
 template <size_t L, typename T, typename U, typename Tag>
-constexpr base<L, T, Tag> cos(base<L, T, Tag> const& a) noexcept {
+[[nodiscard]] inline constexpr base<L, T, Tag> cos(base<L, T, Tag> const& a) noexcept {
     return detail::op_unary(a, [](auto _1) { return std::cos(_1); });
 }
 }
